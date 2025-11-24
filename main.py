9b2b6ed1b527b1a73f6f03c407532ea9cf2ff7fc
yandexlavkacoder <drowka229@gmail.com>
2025-11-24 15:10:35 +0300

    yandexlavkacode

--- main.py
+++ main.py
@@ -1,158 +1,198 @@
 import random
 
-class TicTacToe:
+class TicTacGame:
     def __init__(self):
-        self.player1 = 'X'
-        self.player2 = 'O'
+        self.players = ['X', 'O']
+        self.current_turn = None
+        self.mode = None
     
-    def initialize_board(self, size):
-        return [['.' for _ in range(size)] for _ in range(size)]
+    def make_board(self, n):
+        board = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                row.append('-')
+            board.append(row)
+        return board
     
-    def print_board(self, board):
-        size = len(board)
-        print("  " + " ".join(str(i+1) for i in range(size)))
-        for i in range(size):
-            print(f"{i+1} " + " ".join(board[i]))
+    def show_board(self, board):
+        n = len(board)
+        header = "  "
+        for i in range(n):
+            header += str(i+1) + " "
+        print(header)
+        
+        for i in range(n):
+            line = str(i+1) + " "
+            for j in range(n):
+                line += board[i][j] + " "
+            print(line)
         print()
     
-    def get_valid_size(self):
+    def ask_board_size(self):
         while True:
             try:
-                size = int(input("Enter the size of the board (3-9): "))
-                if 3 <= size <= 9:
-                    return size
-                else:
-                    print("Invalid size, please enter again")
-            except ValueError:
-                print("Please enter a valid number")
+                n = int(input("Board size (3-8): "))
+                if 3 <= n <= 8:
+                    return n
+                print("Size must be between 3 and 8")
+            except:
+                print("Enter a valid number")
     
-    def get_valid_move(self, board, player):
-        size = len(board)
+    def human_turn(self, board, mark):
+        n = len(board)
         while True:
             try:
-                move = input(f"{player}'s turn. Enter row and column (e.g. 1 2): ")
-                row, col = map(int, move.split())
+                move = input(f"Player {mark} move (row col): ")
+                parts = move.split()
+                if len(parts) != 2:
+                    print("Enter two numbers")
+                    continue
+                    
+                r, c = int(parts[0]), int(parts[1])
                 
-                if 1 <= row <= size and 1 <= col <= size:
-                    if board[row-1][col-1] == '.':
-                        return row-1, col-1
-                    else:
-                        print("This cell is already occupied. Choose another one.")
+                if 1 <= r <= n and 1 <= c <= n:
+                    if board[r-1][c-1] == '-':
+                        return r-1, c-1
+                    print("Cell taken")
                 else:
-                    print(f"Please enter numbers between 1 and {size}")
-            except ValueError:
-                print("Please enter two numbers separated by space")
-            except (IndexError, ValueError):
-                print("Invalid input format. Please use: row column (e.g. 1 2)")
+                    print(f"Numbers 1-{n} only")
+                    
+            except:
+                print("Invalid input")
     
-    def get_robot_move(self, board):
-        size = len(board)
-        empty_cells = []
-
-        for i in range(size):
-            for j in range(size):
-                if board[i][j] == '.':
-                    empty_cells.append((i, j))
-
-        if empty_cells:
-            return random.choice(empty_cells)
-        return None
+    def computer_turn(self, board):
+        n = len(board)
+        free_cells = []
+        
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == '-':
+                    free_cells.append((i, j))
+        
+        if free_cells:
+            return random.choice(free_cells)
+        return None, None
     
-    def check_winner(self, board, player):
-        size = len(board)
+    def check_win(self, board, mark):
+        n = len(board)
         
-        
-        for i in range(size):
-            if all(board[i][j] == player for j in range(size)):
+        for i in range(n):
+            win_row = True
+            for j in range(n):
+                if board[i][j] != mark:
+                    win_row = False
+                    break
+            if win_row:
                 return True
-
-        for j in range(size):
-            if all(board[i][j] == player for i in range(size)):
+        
+        for j in range(n):
+            win_col = True
+            for i in range(n):
+                if board[i][j] != mark:
+                    win_col = False
+                    break
+            if win_col:
                 return True
         
-        if all(board[i][i] == player for i in range(size)):
+        win_diag1 = True
+        for i in range(n):
+            if board[i][i] != mark:
+                win_diag1 = False
+                break
+        if win_diag1:
             return True
         
-        if all(board[i][size-1-i] == player for i in range(size)):
+        win_diag2 = True
+        for i in range(n):
+            if board[i][n-1-i] != mark:
+                win_diag2 = False
+                break
+        if win_diag2:
             return True
         
         return False
     
-    def is_board_full(self, board):
-        size = len(board)
-        for i in range(size):
-            for j in range(size):
-                if board[i][j] == '.':
-                    return False
+    def board_full(self, board):
+        for row in board:
+            if '-' in row:
+                return False
         return True
     
-    def choose_game_mode(self):
-        """Выбор режима игры"""
+    def select_mode(self):
+        print("\nGame modes:")
+        print("1 - Two players")
+        print("2 - Vs computer")
+        
         while True:
-            print("\nChoose game mode:")
-            print("1 - Player vs Player")
-            print("2 - Player vs Robot")
-            choice = input("Enter your choice (1 or 2): ")
-            
+            choice = input("Select mode (1/2): ")
             if choice in ['1', '2']:
                 return choice
-            else:
-                print("Invalid choice. Please enter 1 or 2.")
+            print("Enter 1 or 2")
     
-    def choose_starting_player(self):
-        return random.choice([self.player1, self.player2])
+    def pick_first(self):
+        first = random.choice(['X', 'O'])
+        print(f"{first} goes first")
+        return first
     
-    def play_game(self):
+    def play_round(self):
+        
+        self.mode = self.select_mode()
+        
+        size = self.ask_board_size()
+        board = self.make_board(size)
+        
+        if self.mode == '1':
+            self.current_turn = 'X'
+        else:
+            self.current_turn = self.pick_first()
+        
         while True:
-            print("\n=== Tic-Tac-Toe Game ===")
+            self.show_board(board)
             
-            game_mode = self.choose_game_mode()
+            if self.mode == '2' and self.current_turn == 'O':
+                print("Computer thinking...")
+                r, c = self.computer_turn(board)
+                if r is not None:
+                    board[r][c] = 'O'
+                    current_mark = 'O'
+            else:
+                r, c = self.human_turn(board, self.current_turn)
+                board[r][c] = self.current_turn
+                current_mark = self.current_turn
             
-            size = self.get_valid_size()
-            board = self.initialize_board(size)
+            if self.check_win(board, current_mark):
+                self.show_board(board)
+                if self.mode == '2' and current_mark == 'O':
+                    print("Computer wins!")
+                else:
+                    print(f"Player {current_mark} wins!")
+                break
             
-            current_player = self.choose_starting_player()
-            print(f"\n{current_player} starts the game!")
+            if self.board_full(board):
+                self.show_board(board)
+                print("It's a tie!")
+                break
             
-            moves_count = 0
+            if self.mode == '1':
+                self.current_turn = 'O' if self.current_turn == 'X' else 'X'
+            else:
+                self.current_turn = 'O' if self.current_turn == 'X' else 'X'
+    
+    def start_game(self):
+        while True:
+            self.play_round()
             
             while True:
-                self.print_board(board)
-                
-                if game_mode == '2' and current_player == self.player2:
-                    print("Robot's turn...")
-                    row, col = self.get_robot_move(board)
-                else:
-                    row, col = self.get_valid_move(board, current_player)
-                
-                board[row][col] = current_player
-                moves_count += 1
-                
-                if self.check_winner(board, current_player):
-                    self.print_board(board)
-                    result = f"{current_player} wins!"
-                    print(result)
+                again = input("\nPlay again? (y/n): ").lower()
+                if again in ['y', 'n']:
                     break
-                
-                if self.is_board_full(board):
-                    self.print_board(board)
-                    result = "Draw!"
-                    print(result)
-                    break
-                
-                current_player = self.player2 if current_player == self.player1 else self.player1
+                print("Enter y or n")
             
-            while True:
-                play_again = input("\nDo you want to play again? (y/n): ").lower()
-                if play_again in ['y', 'n']:
-                    break
-                else:
-                    print("Please enter 'y' for yes or 'n' for no")
-            
-            if play_again == 'n':
-                print("Thanks for playing!")
+            if again == 'n':
+                print("Game over")
                 break
 
 if __name__ == "__main__":
-    game = TicTacToe()
-    game.play_game()
+    game = TicTacGame()
+    game.start_game()
